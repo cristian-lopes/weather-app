5a0eeed421736da18327881c013015a06faecf61
Cristian Castro Lopes <cristian.lopes@outlook.com>
2025-01-10 17:53:24 +0000

    Subida

---
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,18 @@
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherDatas } from './../../../../models/interfaces/weatherDatas';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+export class WeatherCardComponent  {
+
+  @Input() weatherDatasInput !: WeatherDatas
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+}
